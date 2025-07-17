e685a78635a94cbfeb62a4ba4db07b6a3f27c84c
Kamil Mosciszko <kamil.mazurek319@gmail.com>
2025-07-17 11:19:45 +0200

    Add transport tests

---
+++ graphql_transport_test.go
@@ -0,0 +1,57 @@
+package UTCP
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestGraphQLClientTransport_RegisterAndCall(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var req struct {
+			Query     string                 `json:"query"`
+			Variables map[string]interface{} `json:"variables"`
+		}
+		json.NewDecoder(r.Body).Decode(&req)
+		if strings.Contains(req.Query, "__schema") {
+			w.Header().Set("Content-Type", "application/json")
+			resp := map[string]interface{}{"data": map[string]interface{}{"__schema": map[string]interface{}{
+				"queryType":    map[string]interface{}{"fields": []map[string]interface{}{{"name": "hello", "description": "hi"}}},
+				"mutationType": map[string]interface{}{"fields": []map[string]interface{}{}}}}}
+			json.NewEncoder(w).Encode(resp)
+			return
+		}
+		if strings.Contains(req.Query, "hello") {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(map[string]interface{}{"data": map[string]interface{}{"hello": "world"}})
+			return
+		}
+		http.Error(w, "bad request", http.StatusBadRequest)
+	}))
+	defer server.Close()
+
+	prov := &GraphQLProvider{
+		BaseProvider: BaseProvider{Name: "gql", ProviderType: ProviderGraphQL},
+		URL:          server.URL,
+	}
+	tr := NewGraphQLClientTransport(nil)
+	ctx := context.Background()
+	tools, err := tr.RegisterToolProvider(ctx, prov)
+	if err != nil {
+		t.Fatalf("register error: %v", err)
+	}
+	if len(tools) != 0 {
+		t.Fatalf("unexpected tools: %+v", tools)
+	}
+
+	res, err := tr.CallTool(ctx, "hello", map[string]interface{}{"name": "bob"}, prov, nil)
+	if err != nil {
+		t.Fatalf("call error: %v", err)
+	}
+	if res != "world" {
+		t.Fatalf("unexpected result: %#v", res)
+	}
+}
---
+++ text_client_transport_test.go
@@ -0,0 +1,51 @@
+package UTCP
+
+import (
+	"context"
+	"os"
+	"testing"
+)
+
+func TestTextClientTransport_RegisterAndCall(t *testing.T) {
+	tmp, err := os.CreateTemp("", "manual*.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmp.Name())
+	tmp.WriteString(`{"tools":[{"name":"hello","description":"Hi"}]}`)
+	tmp.Close()
+
+	prov := &TextProvider{
+		BaseProvider: BaseProvider{Name: "text", ProviderType: ProviderText},
+		FilePath:     tmp.Name(),
+	}
+	tr := NewTextTransport("")
+	ctx := context.Background()
+	tools, err := tr.RegisterToolProvider(ctx, prov)
+	if err != nil {
+		t.Fatalf("register error: %v", err)
+	}
+	if len(tools) != 1 || tools[0].Name != "hello" {
+		t.Fatalf("unexpected tools: %+v", tools)
+	}
+
+	tr.tools["hello"] = Tool{Name: "hello", Handler: func(_ map[string]interface{}, _ map[string]interface{}) (map[string]interface{}, error) {
+		return map[string]interface{}{"ok": true}, nil
+	}}
+
+	res, err := tr.CallTool(ctx, "hello", map[string]interface{}{}, prov, nil)
+	if err != nil {
+		t.Fatalf("call error: %v", err)
+	}
+	m := res.(map[string]interface{})
+	if m["ok"] != true {
+		t.Fatalf("unexpected result: %#v", res)
+	}
+
+	if err := tr.DeregisterToolProvider(ctx, prov); err != nil {
+		t.Fatalf("deregister error: %v", err)
+	}
+	if len(tr.tools) != 0 {
+		t.Fatalf("expected tools to be cleared")
+	}
+}
