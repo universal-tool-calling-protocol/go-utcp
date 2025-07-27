f5d26f6fb5a1680ada419a3f16b1fc4b36f7d73a
Kamil Mosciszko <kamil.mazurek319@gmail.com>
2025-07-27 13:29:15 +0200

    feat: fix websocket (#84)

---
+++ src/transports/stream.go
@@ -0,0 +1,34 @@
+package transports
+
+import "io"
+
+type StreamResult interface {
+	Next() (interface{}, error)
+	Close() error
+}
+
+type SliceStreamResult struct {
+	items   []any
+	index   int
+	closeFn func() error
+}
+
+func NewSliceStreamResult(items []any, closeFn func() error) *SliceStreamResult {
+	return &SliceStreamResult{items: items, closeFn: closeFn}
+}
+
+func (sr *SliceStreamResult) Next() (any, error) {
+	if sr.index >= len(sr.items) {
+		return nil, io.EOF
+	}
+	item := sr.items[sr.index]
+	sr.index++
+	return item, nil
+}
+
+func (sr *SliceStreamResult) Close() error {
+	if sr.closeFn != nil {
+		return sr.closeFn()
+	}
+	return nil
+}
